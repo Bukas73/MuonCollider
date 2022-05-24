4f42f60ecef7c45b60704dbb20cfa669e2817b9f
Bukas73 <79034104+Bukas73@users.noreply.github.com>
2022-05-24 12:53:38 -0400

    Add files via upload

--- MuonCollider_Analysis.py
+++ MuonCollider_Analysis.py
@@ -10,9 +10,20 @@ import sys, os
 from scipy import constants
 import math
 from math import pi
+import pickle
+
+from awkward.layout import ListOffsetArray64
+
 
 def convertNBIBToFrac(x):
     return x/2992.
+
+def smear(arr,sigma):
+    #Convert it to a 1D numpy array and perform smearing
+    numpy_arr = np.asarray(arr.layout.content)
+    smeared_arr = np.random.normal(numpy_arr, sigma)
+    #Convert it back to awkward form
+    return ak.Array(ListOffsetArray64(arr.layout.offsets, ak.Array(smeared_arr).layout))
     
 SpeedOfLight = constants.c/1e6 # mm/ns
 
@@ -21,12 +32,12 @@ mpl.rcParams['patch.linewidth']       = 0.5
 mpl.rcParams['savefig.dpi']           = 300
 mpl.rcParams['agg.path.chunksize'] = 10000
 
-colors  = ["#A4036F","#048ba8","#16db93","#efea5a","#f29e4c","#000000","#ffffff"]
+colors  = ["#A4036F","#5213ba","#048ba8","#16db93","#16f421","#efea5a","#f29e4c","#f96e21","#ff1010","#964b00","#000000","#ffffff"]
 markers = ["o","s","D","^","v","<",">","*","X","p"]
 
 
 # hard scatter only (0 BIB)
-noBIBFile = uproot.open(f"lctuple_999.root")
+noBIBFile = uproot.open(f"JetHistograms_All.root")
 noBIBTree = noBIBFile["LCTupleDefault"]
 
 # BIB only
@@ -50,16 +61,25 @@ tmpPz_Squared = np.square(tmpP[3])
 tmpPr = np.sqrt(tmpPx_Squared + tmpPy_Squared)
 tmpMom = np.sqrt(tmpPx_Squared + tmpPy_Squared + tmpPz_Squared)
 
+##Smearing arrays
+ThetaRes = 0 #rad, #Smearing Values: 10 mrad, 25 mrad 50 mrad
+TimeRes = 0 #ns, #Smearing Values: 20 ps, 30ps, 50ps #Also check both smearings at 0.
+tmpNumEntries = tmpPos.size
+
+tmpThetaSmr = np.random.normal(0.0,ThetaRes,tmpNumEntries)
+tmpTimeSmr =  np.random.normal(0.0,TimeRes, tmpNumEntries)
+
 tmpTheta = np.arctan2(tmpPr,tmpP[3])
+tmpTheta = np.add(tmpTheta,tmpThetaSmr)
 tmpThetaEx = np.arctan2(tmpR,tmpX[3])
 tmpThetaDiff = np.subtract(tmpTheta,tmpThetaEx)
 tmpAbsThetaDiff = np.absolute(tmpThetaDiff)
 
 tmpTime = fullBIBTree["sttim"].array()[0]
+tmpTime = np.add(tmpTime,tmpTimeSmr)
 tmpTimeEx = tmpPos/SpeedOfLight
 tmpTimeDiff = np.subtract(tmpTime,tmpTimeEx)
 tmpAbsTimeDiff = np.absolute(tmpTimeDiff)
-
 ### The next chunks of code are to find at which index the if condition is true.
 tmpThetaCut = []
 
@@ -108,11 +128,13 @@ hsPr = np.sqrt(hsPx_Squared + hsPy_Squared)
 hsMom = np.sqrt(hsPx_Squared + hsPy_Squared + hsPz_Squared)
 
 hsTheta = np.arctan2(hsPr,hsP[3])
+#hsTheta = smear(hsTheta,ThetaRes)
 hsThetaEx = np.arctan2(hsR,hsX[3])
 hsThetaDiff = np.subtract(hsTheta,hsThetaEx)
 hsAbsThetaDiff = np.absolute(hsThetaDiff)
 
 hsTime = noBIBTree["sttim"].array()
+#hsTime = smear(hsTime,TimeRes)
 hsTimeEx = hsPos/SpeedOfLight
 hsTimeDiff = np.subtract(hsTime,hsTimeEx)
 hsAbsTimeDiff = np.absolute(hsTimeDiff)
@@ -126,12 +148,16 @@ for event in range(len(hsMom)):
             hsMomCut.append(True)
         else:
             hsMomCut.append(False)
-            
+
+
 hsMomCut = np.array(hsMomCut)
 hsMomCut = np.where(hsMomCut == True)
 hsMomCut = np.array(hsMomCut)
+
+
 ######
 
+hsNumEntries = len(hsMomCut[0]) #Number of sig hits
 
 hsThetaCut = []
 
@@ -173,13 +199,84 @@ SignalThetaDiff = []
 for i in hsMomCut[0]:
     SignalTimeDiff.append(hsTimeDiffComplete[i])
     SignalThetaDiff.append(hsThetaDiffComplete[i])
-
+SignalAbsTimeDiff = np.absolute(SignalTimeDiff)
+SignalAbsThetaDiff = np.absolute(SignalThetaDiff)
 ######
+'''
+#Create ROC ribon
+
+tDeltaValues = np.linspace(0.03,0.3,75)
+thetaDeltaValues = np.linspace(0.01,2.8,100)
+
+#Sort DeltaValues arrays in descending order
+tDeltaValues = np.sort(tDeltaValues)[::-1] 
+thetaDeltaValues = np.sort(thetaDeltaValues)[::-1]
 
 fig,ax = plt.subplots()
+i = 0
+tmpEffDict, hsEffDict = {}, {}
+for tVal in tDeltaValues:
+    j = 0
 
-h=plt.hist2d(tmpThetaDiff, tmpTimeDiff, bins=[30,30])
-plt.xlabel("θ-θ$_{ex}$ [rad]"); plt.ylabel("T-ToF [ns]")
-plt.scatter(SignalThetaDiff,SignalTimeDiff,s=[[5,]*len(SignalTimeDiff)], c=colors[6])
-fig.colorbar(h[3], ax = ax)
+    tmpDelTimeCut = np.where(tmpAbsTimeDiff < tVal)
+    hsDelTimeCut = np.where(SignalAbsTimeDiff < tVal)
+
+    tmpEfficiencyVals = []
+    hsEfficiencyVals  = []
+
+    for thetaVal in thetaDeltaValues: 
+
+        tmpDelThetaCut = np.where(tmpAbsThetaDiff < thetaVal)
+        hsDelThetaCut = np.where(SignalAbsThetaDiff < thetaVal)
+
+        tmpNumKeptEntries = len(np.intersect1d(tmpDelTimeCut,tmpDelThetaCut))
+        hsNumKeptEntries = len(np.intersect1d(hsDelTimeCut,hsDelThetaCut))
+
+
+        tmpEfficiencyVals.append(tmpNumKeptEntries/tmpNumEntries)
+        hsEfficiencyVals.append(hsNumKeptEntries/hsNumEntries)
+
+        j+=1
+
+    tmpEfficiency = np.array(tmpEfficiencyVals)
+    hsEfficiency = np.array(hsEfficiencyVals)
+
+    tmpEffDict[tVal] = tmpEfficiencyVals
+    hsEffDict[tVal] =  hsEfficiencyVals
+
+    plt.plot(hsEfficiency,tmpEfficiency,'-', c=colors[0])#, label=plotlabel) 
+
+    i+=1
+
+pickleoutput = [hsEffDict,tmpEffDict]
+picklefilename = f"EffDict_{TimeRes}ns_{ThetaRes}rad.pickle"
+with open(picklefilename,'wb') as f:
+    pickle.dump(pickleoutput,f)
+
+    
+
+'''
+
+pickleoutput = [SignalTimeDiff,SignalThetaDiff]
+picklefilename = f"New_Hists.pickle"
+with open(picklefilename, 'wb') as f:
+    pickle.dump(pickleoutput,f)
+
+'''
+fig,ax = plt.subplots()
+
+plt.hist(SignalThetaDiff, bins=30)
+plt.xlabel("θ-θ$_{ex}$ [rad]")#; plt.ylabel("T-ToF [ns]") #"θ-θ$_{ex}$ [rad]","T-ToF [ns]"
+#plt.scatter(SignalThetaDiff,SignalTimeDiff,s=[[5,]*len(SignalTimeDiff)], c=colors[6])
+#fig.colorbar(h[1], ax = ax)
 plt.show()
+
+plt.savefig(f"Expected_Hist_Theta")
+'''
+'''
+plt.xlabel("Signal Efficiency"); plt.ylabel("bkg Efficiency")
+plt.yscale('log')
+plt.xlim([0,1])
+plt.legend(loc=4, ncol=2, fontsize='small')
+plt.savefig(f"test_MuonROC_Ribbon_perf2.png")
+'''
